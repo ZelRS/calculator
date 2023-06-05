998a791d075027005552c2ce185720ed2ff1ec23
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-06-05 21:42:32 +0600

    Мастер-шаблон для проекта "Калькулятор"

---
+++ src/main/java/skypro/cource2/hw4/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package skypro.cource2.hw4.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/cource2/hw4/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.cource2.hw4.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
